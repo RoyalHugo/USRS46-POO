1671a217d08c4ec23348e62c30dc547260e06560
Hugo <hugo.imperat@gmail.com>
2024-11-04 11:11:55 +0100

    Création classe Proprietaire

---
+++ Licence/Proprietaire.cs
@@ -0,0 +1,29 @@
+﻿using POO;
+using System;
+
+public class Proprietaire
+{
+	public string Nom;
+	public string Prenom;
+	public List<Bien> Biens;
+
+	public Proprietaire(string Nom, string Prenom, List<Bien> Biens)
+	{
+		this.Nom = Nom;
+		this.Prenom = Prenom;
+		this.Biens = Biens;
+	}
+
+    public override string ToString()
+    {
+        string ToString = String.Format("Nom: {0}\n", this.Nom);
+        ToString += String.Format("Prénom: {0}\n", this.Prenom);
+		ToString += "Biens: ";
+		foreach (Bien b in this.Biens) 
+		{
+			ToString += String.Format(b.Adresse);
+		}
+
+		return ToString;
+    }
+}
