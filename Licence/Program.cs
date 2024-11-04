9c53d8c08ae8bce61d8baebff82c3a65c71a10b8
Hugo <hugo.imperat@gmail.com>
2024-11-04 09:36:45 +0100

    Constructeur Terrain

--- Licence/Program.cs
+++ Licence/Program.cs
@@ -4,11 +4,20 @@ namespace POO
 {
     public class Terrain
     {
-        
-        
-        
-    }
+        public string Adresse;
+        public float Superficie;
+        public int NbCotesCloture;
+        public bool Riviere;
 
+        // Constructeur
+        public Terrain(string adresse, float superficie, int nbcotescloture, bool riviere)
+        {
+            this.Adresse = adresse;
+            this.Superficie = superficie;
+            this.NbCotesCloture = nbcotescloture;
+            this.Riviere = riviere;
+        }
+    }
     public class Program
     {
         public static void Main(string[] args)
