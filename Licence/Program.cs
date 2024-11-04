97c133923b096740a676e5c2cebe728bb3fd1c54
Hugo <hugo.imperat@gmail.com>
2024-11-04 09:59:03 +0100

    Nouvelles valeurs objets

--- Licence/Program.cs
+++ Licence/Program.cs
@@ -6,15 +6,15 @@ namespace POO
     {
         public static void Main(string[] args)
         {
-            Terrain terrain1 = new Terrain("113 Avenue de France, Paris", 50, 6, false);
-            Terrain terrain2 = new Terrain("108 Boulevard Haussmann, Toulouse", 50, 6, false);
-            Terrain terrain3 = new Terrain("8 Rue Clement, Lyon", 50, 6, false);
+            Terrain terrain1 = new Terrain("113 Avenue de France, Paris", 10, 6, false);
+            Terrain terrain2 = new Terrain("108 Boulevard Haussmann, Toulouse", 39, 4, false);
+            Terrain terrain3 = new Terrain("8 Rue Clement, Lyon", 28, 2, false);
 
             Terrain[] allterrains = { terrain1, terrain2, terrain3 };
 
             foreach (Terrain terrain in allterrains)
             {
-                Console.WriteLine(terrain);
+                Console.WriteLine(terrain + "\n");
             }
         }
     }
